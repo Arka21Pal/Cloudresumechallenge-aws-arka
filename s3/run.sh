57fc879150fa51f6bb88694f12001a67f60b2457
Arka21Pal <write2arkapal@gmail.com>
2022-12-13 20:03:52 -0600

    feat: script to automate preliminary actions for s3
    Commands to utilise the Cloudformation template and AWS commands to
    interact with the s3 bucket, as per instructions of the exercise

--- s3/run.sh
+++ s3/run.sh
@@ -2,29 +2,122 @@
 
 # This file will contain commands to deploy the cloudformation template for s3 and the relevant commands to host a static website.
 
-profile="test-profile"
-region="us-east-1"
-stack="test-stack"
-template_body="file://s3_draft.yaml"
-template="s3_draft.yaml"
-excluded_directory="./.git/*"
-bucket="resume-bucket-unique"
-source_files="../../resume/"
-target="s3://${bucket}"
-# target=$(printf "%s%s" "s3://" "${bucket}")
+cloudformationstackactions() {
 
-# To validate
-aws cloudformation validate-template --region "${region}" --template-body "${template_body}" --profile "${profile}"
+    # Help function as an overview of the capabilities of the script
+    help() {
+        printf "\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" "Here are the various flags supported by the script" \
+            "To validate the template, use flag \"-v\"" \
+            "To deploy the template, use flag \"-d\"" \
+            "To push objects to the existing bucket, use flag \"-p\"" \
+            "To check if the bucket contains any objects, use flag \"-c\"" \
+            "To empty the bucket, use flag \"-e\"" \
+            "To delete the stack (and associated resources), use flag \"-D\""
+    }
 
-# To deploy
-# aws cloudformation deploy --template "${template}" --stack-name "${stack}" --region "${region}" --profile "${profile}"
+    # Invoke the "help" function when used without flags and arguments
+    if [ $# -eq 0 ]; then
+        help
+        return
+    fi
 
 
-# To push to s3 bucket
-# aws s3 cp "${source_files}" "${target}" --exclude "${excluded_directory}" --recursive --region "${region}" --profile "${profile}"
+    # Logic for the flags
 
-# To empty the s3 bucket
-aws s3 rm s3://"${bucket}" --recursive --region "${region}" --profile "${profile}"
+    validate_template=0     # -v
+    deploy_template=0       # -d
+    push_to_bucket=0        # -p
+    check_bucket=0          # -c
+    empty_bucket=0          # -e
+    delete_stack=0          # -D
 
-# To delete
-aws cloudformation delete-stack --stack-name "${stack}" --region "${region}" --profile "${profile}"
+    # In this case, order of commands is very important, as I won't be able to push objects to a bucket without deploying the stack first,
+    # Neither will I be able to delete the stack without emptying the bucket first.
+    # The arguments are parsed in the ORDER OF THE CASE STATEMENTS/the order in the which opts is defined
+
+    while getopts "vdpeDch" opts
+    do
+        case ${opts} in
+            v)
+                validate_template=1
+                ;;
+            d)
+                deploy_template=1
+                ;;
+            p)
+                push_to_bucket=1
+                ;;
+            c)
+                check_bucket=1
+                ;;
+            e)
+                empty_bucket=1
+                ;;
+            D)
+                delete_stack=1
+                empty_bucket=1
+                ;;
+            h)
+                help
+                return
+                ;;
+            \?)
+                printf "\n%s" "Invalid character. Exiting..."
+                return
+                ;;
+            *)
+                printf "\n%s" "Sorry, wrong argument"
+                return
+                ;;
+        esac
+    done
+
+    profile="test-profile"
+    region="us-east-1"
+    stack="test-stack"
+    template_body="file://s3_draft.yaml"
+    template="s3_draft.yaml"
+    excluded_directory="./.git/*"
+    bucket="resume-bucket-unique"
+    source_files="../../resume/"
+    target="s3://${bucket}"
+    # target=$(printf "%s%s" "s3://" "${bucket}")
+
+    if [ "${validate_template}" = 1 ]; then
+
+        # To validate the template
+        aws cloudformation validate-template --region "${region}" --template-body "${template_body}" --profile "${profile}"
+    fi
+
+    if [ "${deploy_template}" = 1 ]; then
+
+        # To deploy the stack
+        aws cloudformation deploy --template "${template}" --stack-name "${stack}" --region "${region}" --profile "${profile}"
+    fi
+
+    if [ "${push_to_bucket}" = 1 ]; then
+
+        # To push to s3 bucket
+        aws s3 cp "${source_files}" "${target}" --exclude "${excluded_directory}" --recursive --region "${region}" --profile "${profile}"
+    fi
+
+    if [ "${empty_bucket}" = 1 ]; then
+
+        # To empty the s3 bucket
+        aws s3 rm s3://"${bucket}" --recursive --region "${region}" --profile "${profile}"
+    fi
+
+    if [ "${delete_stack}" = 1 ]; then
+
+        # To delete stack and all resources with it
+        aws cloudformation delete-stack --stack-name "${stack}" --region "${region}" --profile "${profile}"
+    fi
+
+    if [ "${check_bucket}" = 1 ]; then
+
+        # To check contents of bucket
+        aws s3 ls s3://"${bucket}" --recursive --human-readable --summarize --region "${region}" --profile "${profile}"
+    fi
+}
+
+cloudformationstackactions "$@"
