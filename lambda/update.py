77e1d10cc73b560407a4f395d063629e0b2659ae
Arka21Pal <write2arkapal@gmail.com>
2022-12-24 14:01:35 -0500

    refactor: remove preliminary code

--- lambda/update.py
+++
@@ -1,2 +0,0 @@
-# Tutorial: https://www.section.io/engineering-education/python-boto3-and-amazon-dynamodb-programming-tutorial/
-# To reach DynamoDB endpoint: https://docs.aws.amazon.com/general/latest/gr/ddb.html
