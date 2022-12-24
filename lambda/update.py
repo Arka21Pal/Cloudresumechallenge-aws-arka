a0f8619bb47a73bcd183e1f9cae2fbb4ce509981
Arka21Pal <88698298+Arka21Pal@users.noreply.github.com>
2022-12-23 20:38:14 -0600

    docs: update.py comments
    Two articles, one for a tutorial and another for a list of defined DynamoDB endpoints

---
+++ lambda/update.py
@@ -0,0 +1,2 @@
+# Tutorial: https://www.section.io/engineering-education/python-boto3-and-amazon-dynamodb-programming-tutorial/
+# To reach DynamoDB endpoint: https://docs.aws.amazon.com/general/latest/gr/ddb.html
