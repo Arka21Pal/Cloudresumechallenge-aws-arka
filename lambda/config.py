f3210b4501497baac7302bdc26e0dc0ab8b5e5a5
Arka21Pal <write2arkapal@gmail.com>
2022-12-28 02:11:49 -0500

    refactor: change directory, delete config.py
    Have new way (recorded in the note.md) to access credentials

--- lambda/config.py
+++
@@ -1,5 +0,0 @@
-#!/bin/python
-
-aws_access_key_id=
-aws_secret_access_key=
-region=
---
+++ lambda/drafts/show.py
@@ -0,0 +1,45 @@
+#!/bin/python
+
+import json
+import datetime
+import boto3
+import config
+
+TABLE_NAME = "visitor-count"
+
+resource = boto3.resource("dynamodb", endpoint_url="http://dynamodb.us-east-1.amazonaws.com", aws_access_key_id=config.aws_access_key_id, aws_secret_access_key=config.aws_secret_access_key, region_name=config.region)
+
+table = resource.Table(TABLE_NAME)
+
+for i in range(5):
+
+    def put_item(TABLE):
+        response = TABLE.put_item(
+                # Data to be inserted
+                Item={
+                    "Count": 1,
+                    "Number": 1,
+                    "Time": str(datetime.datetime.now().time()),
+                    "Visits": i
+                }
+            )
+
+        # https://stackoverflow.com/a/56061155
+        print(json.dumps(response, indent=2, default=str))
+
+    def show_item(TABLE):
+        response = TABLE.get_item(
+                TableName=TABLE_NAME,
+                Key={
+                    "Count": 1,
+                    "Number": 1
+                    }
+                )
+
+        # https://stackoverflow.com/a/56061155
+        print(json.dumps(response["Item"], indent=2, default=str))
+#         print(json.dumps(response["Item"]["Visits"], indent=2, default=str))
+
+    put_item(table)
+    show_item(table)
+
