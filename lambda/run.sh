c076f14cc5b0d332be6cda68a52222d626e5d99c
Arka21Pal <write2arkapal@gmail.com>
2023-01-01 12:45:36 -0500

    feat: integrate "s3.run.sh" to work with both templates (and stacks)
    feat: introduce a flag to update a function
    refactor: miscellaneous changes, CAPABILITY_NAMED_IAM

--- lambda/run.sh
+++ lambda/run.sh
@@ -6,12 +6,10 @@ cloudformationstackactions() {
 
     # Help function as an overview of the capabilities of the script
     help() {
-        printf "\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" "Here are the various flags supported by the script" \
+        printf "\n%s\n%s\n%s\n%s\n%s\n%s" "Here are the various flags supported by the script" \
             "To validate the template, use flag \"-v\"" \
             "To deploy the template, use flag \"-d\"" \
-            "To push objects to the existing bucket, use flag \"-p\"" \
-            "To check if the bucket contains any objects, use flag \"-c\"" \
-            "To empty the bucket, use flag \"-e\"" \
+            "To update the stack, use the flag \"-u\"" \
             "To delete the stack (and associated resources), use flag \"-D\"" \
             "To retain the log of the process in a file (mention file as argument after flag), use flag \"-l\""
     }
@@ -22,16 +20,11 @@ cloudformationstackactions() {
         return
     fi
 
-    # Get the argument specified (the logic is that the word required will be the last argument)
-#     word="$(for list in "$@"; do : ; done ; printf "%s" "${list}")"
-
     # Logic for the flags
 
     validate_template=0     # -v
     deploy_template=0       # -d
-    push_to_bucket=0        # -p
-    check_bucket=0          # -c
-    empty_bucket=0          # -e
+    update_stack=0          # -u
     delete_stack=0          # -D
     retain_log=0            # -l
 
@@ -39,7 +32,7 @@ cloudformationstackactions() {
     # Neither will I be able to delete the stack without emptying the bucket first.
     # The arguments are parsed in the ORDER OF THE CASE STATEMENTS/the order in the which opts is defined
 
-    while getopts "vdpeDclh" opts
+    while getopts "vdupDlh" opts
     do
         case ${opts} in
             v)
@@ -48,21 +41,14 @@ cloudformationstackactions() {
             d)
                 deploy_template=1
                 ;;
-            p)
-                push_to_bucket=1
-                ;;
-            c)
-                check_bucket=1
-                ;;
-            e)
-                empty_bucket=1
-                ;;
             D)
                 delete_stack=1
-                empty_bucket=1
                 ;;
             l)
                 retain_log=1
+                ;;
+            u)
+                update_stack=1
                 ;;
             h)
                 help
@@ -81,75 +67,75 @@ cloudformationstackactions() {
 
     profile="test-profile"
     region_name="us-east-1"
-    stack="test-stack"
+    stack_name="test-stack"
+    table_name="visitor-count"
 
-    template1="lambda.yaml"
-    template2="bucket.yaml"
-    template_body1="file://${template1}"
-    template_body2="file://${template2}"
-
-    bucket="${stack}-bucket-unique"
-    source_file="code.py"
-    zip_file="code.zip"
-
-    target="s3://${bucket}"
-    # target=$(printf "%s%s" "s3://" "${bucket}")
+    template="lambda.yaml"
+    template_body="file://${template}"
 
     function_name="LambdaFunction"
+
     aws_access_key_id="$(aws configure get aws_access_key_id)"
     aws_secret_access_key=$(aws configure get aws_secret_access_key)
+
+    change_set="change-set"
+    capabilities="CAPABILITY_NAMED_IAM"
 
     if [ "${validate_template}" = 1 ]; then
 
         # To validate the template
-        aws cloudformation validate-template --region "${region_name}" --template-body "${template_body1}" --profile "${profile}"
-        aws cloudformation validate-template --region "${region_name}" --template-body "${template_body2}" --profile "${profile}"
+        aws cloudformation validate-template --region "${region_name}" --template-body "${template_body}" --profile "${profile}"
     fi
 
     if [ "${deploy_template}" = 1 ]; then
 
+        # Run helper script to deploy bucket and push zip of code to it
+        ./s3run.sh -d
+
         # To deploy the stack
-
-        aws cloudformation deploy --template "${template2}" --stack-name "${stack}" --region "${region_name}" --profile "${profile}"
-
-        # Requires explicit mention of "--capabilities CAPABILITY_NAMED_IAM" since we create a role in this template
-        # https://docs.aws.amazon.com/AWSCloudFormation/latest/APIReference/API_CreateStack.html
-        aws cloudformation deploy --template "${template1}" --stack-name "${stack}" --region "${region_name}" --profile "${profile}" --capabilities CAPABILITY_NAMED_IAM
-
+        aws cloudformation deploy --template "${template}" --stack-name "${stack_name}" --region "${region_name}" --profile "${profile}" --capabilities "${capabilities}"
 
         # Set environment variables
-        aws lambda update-function-configuration --function-name "${function_name}" --environment \"Variables=\{TABLE_NAME=visitor-count, AWS_ACCESS_KEY_ID="${aws_access_key_id}", AWS_SECRET_ACCESS_KEY="${aws_secret_access_key}", REGION_NAME="${region_name}"\}\" --region ${region_name} --profile "${profile}"
+        aws lambda update-function-configuration --function-name "${function_name}" --environment "Variables={table_name='${table_name}', aws_access_key_id='${aws_access_key_id}', aws_secret_access_key='${aws_secret_access_key}', region_name='${region_name}'}" --region "${region_name}" --profile "${profile}"
     fi
 
-    if [ "${push_to_bucket}" = 1 ]; then
+    if [ "${update_stack}" = 1 ]; then
 
-        # To push to s3 bucket
-        zip "${zip_file}" "${source_file}"
-        aws s3 cp "${zip_file}" "${target}" --region "${region_name}" --profile "${profile}"
-    fi
+        # Create change-set
+        aws cloudformation create-change-set \
+            --stack-name "${stack_name}" \
+            --change-set-name "${change_set}" \
+            --template-body "${template_body}" \
+            --capabilities "${capabilities}"
 
-    if [ "${empty_bucket}" = 1 ]; then
+        # List the change-set available for the stack
+        aws cloudformation list-change-sets --stack-name "${stack_name}"
 
-        # To empty the s3 bucket
-        aws s3 rm s3://"${bucket}" --recursive --region "${region_name}" --profile "${profile}"
+        # Describe the changes listed in the change-set
+        aws cloudformation describe-change-set --change-set-name "${change_set}"
+
+        # Execute change-set
+        aws cloudformation execute-change-set \
+            --change-set-name "${change_set}" \
+            --stack-name "${stack_name}"
+
+        # Describe the stack after the change
+        aws cloudformation describe-stacks --stack-name "${stack_name}" | less -FXNR
     fi
 
     if [ "${delete_stack}" = 1 ]; then
 
+        # Run helper script to empty bucket before deleting stack
+        ./s3run.sh -e
+
         # To delete stack and all resources with it
-        aws cloudformation delete-stack --stack-name "${stack}" --region "${region_name}" --profile "${profile}"
-    fi
-
-    if [ "${check_bucket}" = 1 ]; then
-
-        # To check contents of bucket
-        aws s3 ls s3://"${bucket}" --recursive --human-readable --summarize --region "${region_name}" --profile "${profile}"
+        aws cloudformation delete-stack --stack-name "${stack_name}" --region "${region_name}" --profile "${profile}"
     fi
 
     if [  "${retain_log}" = 1 ]; then
 
         # Write events to file (mentioned as argument)
-        aws cloudformation describe-stack-events --stack-name "${stack}" --region "${region_name}" --profile "${profile}" >> logfile
+        aws cloudformation describe-stack-events --stack-name "${stack_name}" --region "${region_name}" --profile "${profile}" >> logfile
     fi
 }
 
