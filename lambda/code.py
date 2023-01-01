29fecdd63ed0dd7ddce9d844008561b521ce2bc5
Arka21Pal <write2arkapal@gmail.com>
2023-01-01 12:51:42 -0500

    feat: utilise a function to specify as "Handler" in template
    refactor: catch errors, invoke functions to work with Lambda
    pending: utilise "event" and "context" properly once the API response is
    established

--- lambda/code.py
+++ lambda/code.py
@@ -1,24 +1,40 @@
 #!/bin/python
 
+"""Python file to get and update value of visitors in DynamoDB table"""
+
+import sys
+import traceback
 import json
 import datetime
 import os
 import boto3
 
-# TABLE_NAME = "visitor-count"
-TABLE_NAME = os.environ.get("TABLE_NAME")
 
-aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-region_name = os.environ.get("REGION_NAME")
+def log_exception():
+    """Log a stack trace"""
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    print(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
 
-resource = boto3.resource("dynamodb", endpoint_url="http://dynamodb.us-east-1.amazonaws.com", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
 
-table = resource.Table(TABLE_NAME)
+# table_name = "visitor-count"
+table_name = os.environ.get("table_name")
+
+aws_access_key_id = os.environ.get("aws_access_key_id")
+aws_secret_access_key = os.environ.get("aws_secret_access_key")
+region_name = os.environ.get("region_name")
+
+resource = boto3.resource("dynamodb",
+        endpoint_url="http://dynamodb.us-east-1.amazonaws.com",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name=region_name)
+
+TABLE = resource.Table(table_name)
 
 def show_item(TABLE):
+    """Function to get items and return output from DynamoDB table"""
     response = TABLE.get_item(
-            TableName=TABLE_NAME,
+            TableName=table_name,
             Key={
                 "Count": 1,
                 "Number": 1
@@ -28,11 +44,13 @@ def show_item(TABLE):
 #     print(json.dumps(response["Item"], indent=2, default=str))
     visits = int(json.dumps(response["Item"]["Visits"], indent=2, default=str))
 
-    if visits == 0: visits = 1
+    if visits == 0:
+        visits = 1
 
     return visits
 
 def put_item(TABLE):
+    """Function to put item/update item in DynamoDB table"""
     response = TABLE.put_item(
             # Data to be inserted
             Item={
@@ -46,9 +64,16 @@ def put_item(TABLE):
     # https://stackoverflow.com/a/56061155
     return json.dumps(response, indent=2, default=str)
 
-for i in range(5):
-    put_item(table)
+def lambda_handler(TABLE):
+    """Lambda handler for the custom resource"""
+    try:
+        i = 5
+        while i:
+            put_item(TABLE)
+            i -= 1
 
-print(show_item(table))
+        print(show_item(TABLE))
 
-# show_item(table)
+    except Exception:
+        log_exception()
+        raise
