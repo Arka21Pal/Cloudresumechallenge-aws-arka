7d61949b47018d65313d61690ef7c6393e3e3b6a
Arka21Pal <write2arkapal@gmail.com>
2023-01-23 02:08:06 -0500

    refactor: CORS configuration in Lambda code

--- backend/dynamodb.py
+++ backend/dynamodb.py
@@ -55,7 +55,12 @@ def table_operations():
             "statusCode": 200,
             "body": str(current_visits),
             "headers": {
-                "content-type": "application/json"
+                "Content-Type" : "application/json",
+                "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                "Access-Control-Allow-Methods" : "OPTIONS,GET",
+                "Access-Control-Allow-Credentials" : "true",
+                "Access-Control-Allow-Origin" : "*",
+                "X-Requested-With" : "*"
                 }
             }
 
